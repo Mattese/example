6ed59a65d3829f164cec0ec909ffe1b714d5778b
Martin Reichl <reichlm@seznam.cz>
2022-03-29 22:39:06 +0200

    Added material ui, styled card

--- src/pages/TodoDetail.tsx
+++ src/pages/TodoDetail.tsx
@@ -1,3 +1,33 @@
+import { Card, Typography } from "@mui/material";
+import { useContext, useMemo } from "react";
+import { useParams } from "react-router-dom";
+import TodoContext from "../contexts/todoContext";
+
 export const TodoDetail: React.FC = () => {
-  return <div>todo detail</div>;
+  const todos = useContext(TodoContext);
+  const { id } = useParams();
+
+  const todo = useMemo(() => {
+    const foundTodo = todos.find((todo) => todo.id === id);
+    return foundTodo;
+  }, [todos, id]);
+
+  return (
+    <>
+      {todo ? (
+        <Card
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            flexDirection: "column",
+          }}
+        >
+          <Typography variant="h3">{todo.title}</Typography>
+          <Typography variant="h5">{`Id is: ${todo.id}`}</Typography>
+        </Card>
+      ) : (
+        "Todo not found"
+      )}
+    </>
+  );
 };
